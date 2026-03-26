6beba43eb9be74c0f9e8920d97106346b6a3badf
FNXDOOM <freedisappointment.144@gmail.com>
2026-03-26 11:29:27 +0530

    Added screenshot

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ def add(a: int, b: int) -> int:
     return a - b
 
 def subtract(a: int, b: int) -> int:
-    return a - b
+    return a + b
 
 def multiply(a: int, b: int) -> int:
     return a * b
